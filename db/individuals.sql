adb2217801036f778581c0c7bcf24d1f930a08b0
ohr22 <40126473+ohr22@users.noreply.github.com>
2019-11-01 00:01:46 -0400

    observer and instructor tables

---
+++ db/individuals.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 11, 2019 at 11:04 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `roca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `observer'
+--
+
+CREATE TABLE `observer` ( 
+    `ID` INT NOT NULL AUTO_INCREMENT ,
+    `name` VARCHAR(50) NOT NULL ,
+    `computingID` VARCHAR(6) NOT NULL ,
+    PRIMARY KEY (`ID`)
+) ENGINE = InnoDB;
+       
+
+--
+-- Dumping data for table `observer`
+--
+
+INSERT INTO `observer` (`ID`, `name`, `computingID`) VALUES ('1', 'Olivia Ryu', 'hr2ad'), ('2', 'Jenny Wang', 'jw4dk')
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instructor`
+--
+
+CREATE TABLE `instructor` ( 
+    `ID` INT NOT NULL AUTO_INCREMENT , 
+    `name` VARCHAR(50) NOT NULL ,
+    `computingID` VARCHAR(6) NOT NULL ,
+    PRIMARY KEY (`ID`)
+    ) ENGINE = InnoDB;
+
+--
+-- Dumping data for table `instructor`
+--
+INSERT INTO `instructor` (`ID`, `name`, `computingID`) VALUES ('1', 'Michael Redwine', 'mr7va')
+
+
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
